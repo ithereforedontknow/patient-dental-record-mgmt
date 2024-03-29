6c7dfc77b280c81633a76d4a4725b6d102ce357e
asdf <smth@email.com>
2024-03-29 12:29:28 +0800

    database

---
+++ db_dental.sql
@@ -0,0 +1,38 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 100428 (10.4.28-MariaDB)
+ Source Host           : localhost:3306
+ Source Schema         : db_dental
+
+ Target Server Type    : MySQL
+ Target Server Version : 100428 (10.4.28-MariaDB)
+ File Encoding         : 65001
+
+ Date: 29/03/2024 12:29:18
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for tbl_users
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_users`;
+CREATE TABLE `tbl_users`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `userlevel` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2404 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tbl_users
+-- ----------------------------
+INSERT INTO `tbl_users` VALUES (2401, 'admin', 'admin');
+INSERT INTO `tbl_users` VALUES (2402, 'dentist', 'dentist');
+INSERT INTO `tbl_users` VALUES (2403, 'test', 'patient');
+
+SET FOREIGN_KEY_CHECKS = 1;
